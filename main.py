b232d7fa8a26b96e16403b5180d5658551d2c446
Rafale25 <narchalpaul@yahoo.fr>
2021-03-23 10:31:25 +0100

    update

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 #! /usr/bin/python3
 
+import sys
 from boids import MyWindow
 
 def main():
 
+	nb_boids = int(sys.argv[1])
+
 	app = MyWindow(
-		max_boids=30000,
+		max_boids=nb_boids,
 		map_size=50,
-		width=1920,
-		height=1080,
+		width=1280,
+		height=720,
 		caption="Boids Simulation 3D",
 		fullscreen=False,
-		resizable=False,
+		resizable=True,
 		vsync=False
 	).run()
 
