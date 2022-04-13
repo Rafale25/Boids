5567411e59c19902f1b059953e04ec9cca80e3fd
Rafale25 <narchalpaul@yahoo.fr>
2022-04-14 01:04:33 +0200

    update

--- src/boids.py
+++ src/boids.py
@@ -27,7 +27,7 @@ class MyWindow(moderngl_window.WindowConfig):
     window_size = (1920, 1080)
     fullscreen = False
     resizable = True
-    vsync = False
+    vsync = True
     resource_dir = (Path(__file__) / "../../assets").resolve()
 
     def __init__(self, **kwargs):
@@ -39,10 +39,10 @@ class MyWindow(moderngl_window.WindowConfig):
         self.local_size_x = 512 ## smaller value is better when boids are close to each others, and bigger when they are far appart
         self.min_boids = self.local_size_x
         self.max_boids = 2**21#self.local_size_x * 150
-        self.map_size = 90
+        self.map_size = 120
         self.map_type = MapType.MAP_CUBE
 
-        self.boid_count = 2**19 ## must be a power of 2 or it the sort will not work
+        self.boid_count = 2**21 ## must be a power of 2 or it the sort will not work
         self.view_angle = pi/2
         self.view_distance = 2.0
         self.speed = 0.0 #0.050
