1143bf7fd73d40e5613d7399e7b06ac893bec777
Rafale25 <narchalpaul@yahoo.fr>
2022-05-03 20:51:41 +0200

    update

--- src/_resize_buffer.py
+++ src/_resize_buffer.py
@@ -28,28 +28,3 @@ def resize_boids_buffer(self, new_count):
     self.buffer_cell_count_2.orphan(new_count * 4)
 
     self.boid_count = new_count
-
-
-# from array import array
-# def resize_boids_buffer(self, new_count):
-#     read_size = min(new_count, self.boid_count)
-#     bytes1 = self.buffer_boid.read()[0:read_size * 32]
-#     bytes2 = self.buffer_boid_tmp.read()[0:read_size * 32]
-#     ## orphaning buffer don't garant
-#
-#     self.buffer_boid.orphan(new_count * 32)
-#     self.buffer_boid_tmp.orphan(new_count * 32)
-#
-#     self.buffer_cell_count_1.orphan(new_count * 4)
-#     self.buffer_cell_count_2.orphan(new_count * 4)
-#
-#     if new_count > self.boid_count:
-#         b_new_boids = array('f', self.gen_initial_data(new_count - self.boid_count))
-#
-#         bytes1 += b_new_boids
-#         bytes2 += b_new_boids
-#
-#     self.buffer_boid.write(bytes1)
-#     self.buffer_boid_tmp.write(bytes2)
-#
-#     self.boid_count = new_count
