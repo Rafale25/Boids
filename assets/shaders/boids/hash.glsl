f59affed68608e894620d9370c689410f1d724cf
Rafale25 <narchalpaul@yahoo.fr>
2022-05-06 02:39:23 +0200

    morton-code (unused)

--- assets/shaders/boids/hash.glsl
+++ assets/shaders/boids/hash.glsl
@@ -9,6 +9,30 @@ uint hash(ivec3 cell_index, uint count) {
     return i % count;
 }
 
+// uint expandBits(uint v)
+// {
+//     v = (v * 0x00010001u) & 0xFF0000FFu;
+//     v = (v * 0x00000101u) & 0x0F00F00Fu;
+//     v = (v * 0x00000011u) & 0xC30C30C3u;
+//     v = (v * 0x00000005u) & 0x49249249u;
+//     return v;
+// }
+//
+// uint hash(ivec3 cell_index, uint count)
+// {
+//     const int MAX_SIZE = 161;//int(pow(count, 1.0/3.0));
+//     cell_index.x %= MAX_SIZE;
+//     cell_index.y %= MAX_SIZE*MAX_SIZE;
+//     cell_index.z %= MAX_SIZE*MAX_SIZE*MAX_SIZE;
+//     // x = min(max(x * 1024.0f, 0.0f), 1023.0f);
+//     // y = min(max(y * 1024.0f, 0.0f), 1023.0f);
+//     // z = min(max(z * 1024.0f, 0.0f), 1023.0f);
+//     uint xx = expandBits(uint(cell_index.x));
+//     uint yy = expandBits(uint(cell_index.y));
+//     uint zz = expandBits(uint(cell_index.z));
+//     return (xx * 4 + yy * 2 + zz) % count;
+// }
+
 // z-order curve
 /*
 uint hash(ivec3 cell_index, uint count) {
